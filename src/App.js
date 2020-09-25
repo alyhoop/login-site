a8cc6388a08a9c53bd8950ee852d26119209c939
Alyssa Hooper <nesteralyssa@gmail.com>
2020-09-25 15:32:28 -0700

    adds folders and first component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
 import React from 'react';
+import Header from './components/header/header';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Hi
-        </p>
-      </header>
+      <Header />
     </div>
   );
 }
