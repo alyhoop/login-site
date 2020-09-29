bfa768d4f6af2128ab0986e63a7c32538ce9ba41
Alyssa Hooper <nesteralyssa@gmail.com>
2020-09-28 21:25:10 -0700

    routing update

--- src/App.js
+++ src/App.js
@@ -1,12 +1,42 @@
-import React from 'react';
-import Header from './components/header/header';
+import React, {useState} from 'react';
 import './App.css';
-
+import Header from './components/header/header';
+import LoginForm from './components/login-form/login-form';
+import RegistrationForm from './components/registration-form/registration-form';
+import Home from './components/home/home';
+import PrivateRoute from './utils/PrivateRoute';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
+import AlertComponent from './components/alert-component/alert-component';
 function App() {
+  const [title, updateTitle] = useState(null);
+  const [errorMessage, updateErrorMessage] = useState(null);
   return (
+    <Router>
     <div className="App">
-      <Header />
+      <Header title={title}/>
+        <div className="container d-flex align-items-center flex-column">
+          <Switch>
+            <Route path="/" exact={true}>
+              <RegistrationForm showError={updateErrorMessage} updateTitle={updateTitle}/>
+            </Route>
+            <Route path="/register">
+              <RegistrationForm showError={updateErrorMessage} updateTitle={updateTitle}/>
+            </Route>
+            <Route path="/login">
+              <LoginForm showError={updateErrorMessage} updateTitle={updateTitle}/>
+            </Route>
+            <PrivateRoute path="/home">
+              <Home/>
+            </PrivateRoute>
+          </Switch>
+          <AlertComponent errorMessage={errorMessage} hideError={updateErrorMessage}/>
+        </div>
     </div>
+    </Router>
   );
 }
 
